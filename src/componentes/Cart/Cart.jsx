9b4b77746a218a227dfa07046ef548b6595e6874
JonathanBonnie <jg_uba@hotmail.com>
2023-03-18 04:21:13 -0300

    UNA VEZ MAS

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useDarkModeContext } from "../../context/DarkModeContext";
 import { useCarritoContext } from "../../context/CarritoContex";
 
-const Cart = () => {
+const Cart = () => {  
     const { darkMode } = useDarkModeContext() 
     const { carrito, emptyCart, totalPrice, removeItem } = useCarritoContext()
 
